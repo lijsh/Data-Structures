7e9a0eca416bb67fbe91955070eb38ee1efd1a93
lijsh <sysulijs@gmail.com>
2017-02-08 13:46:48 +0800

    modify hashTable class

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -1,27 +1,19 @@
+// 分离链接实现
 const LENGTH_OF_ARRAY = 137
+const hashInit = 5381
+const hashFactor = 33
 
 class HashTable {
   constructor() {
     this.table = new Array(LENGTH_OF_ARRAY)
-      .join()
-      .split(',')
-      .map(() => [])
   }
 
   put(key, data) {
     const pos = HashTable.hashify(key, this.table.length)
-    const target = this.table[pos]
-    let index = 0
-    if (target[index] === undefined) {
-      target[index] = key
-      target[index + 1] = data
-    } else {
-      while (target[index] !== undefined) {
-        index += 1
-      }
-      target[index] = key
-      target[index + 1] = data
+    if (this.table[pos] === undefined) {
+      this.table[pos] = []
     }
+    this.table[pos].push(key, data)
   }
 
   get(key) {
@@ -29,24 +21,16 @@ class HashTable {
       key = key.toString()
     }
     const pos = HashTable.hashify(key, this.table.length)
+    if (this.table[pos] === undefined) return
     const index = this.table[pos].indexOf(key)
     return index > -1 ? this.table[pos][index + 1] : undefined
   }
 
-  show() {
-    this.table.forEach((v) => {
-      if (v.length > 0) {
-        console.log(v)
-      }
-    })
-  }
-
   static hashify(key, num) {
-    const H = 37
     if (typeof key !== 'string') {
       key = key.toString()
     }
-    const total = key.split('').reduce((a, b, i) => (a * H) + a + key.charCodeAt(i), 0)
+    const total = key.split('').reduce((a, b, i) => (a * hashFactor) + a + key.charCodeAt(i), hashInit)
     return total % num
   }
 }
